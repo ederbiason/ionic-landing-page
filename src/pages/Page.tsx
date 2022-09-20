abf0777384da1e5bc2524f7e2134003e08195236
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-09-20 11:56:59 -0300

    Page component
    Setting page component

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,6 +1,7 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { searchOutline, searchSharp, bagAddSharp, bagAddOutline } from 'ionicons/icons';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
+import LandingPage from '../components/LandingPage';
 import './Page.css';
 
 const Page: React.FC = () => {
@@ -15,16 +16,20 @@ const Page: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>{name}</IonTitle>
+          <IonButtons slot="end">
+            <IonIcon ios={searchSharp} md={searchOutline}/>
+            <IonIcon ios={bagAddSharp} md={bagAddOutline}/>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
 
-      <IonContent fullscreen>
+      <IonContent fullscreen class="page">
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
+        <LandingPage />
       </IonContent>
     </IonPage>
   );
